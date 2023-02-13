edb4ea54fc9a15feb3364f954f0cdc5f76672417
MarcinZbor <marcinzb@gazeta.pl>
2023-02-13 14:07:06 +0100

    Commit3

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -8,14 +8,14 @@ galleryContainer.insertAdjacentHTML("afterbegin", imageMarkup);
 function createItemsMarkup(images) {
   return images
     .map(({ preview, original, description }) => {
-      return;
-      `<li class = "gallery__item">
+      return`<li class = "gallery__item">
     <a class="gallery__link" href = ${original}>
     <img class="gallery__image" src = ${preview}
     alt = ${description} />
         </a>
         </li>`;
     })
+
     .join("");
 }
 const lightbox = new SimpleLightbox(".gallery a", {
