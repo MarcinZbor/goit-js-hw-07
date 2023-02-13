d15334eee859cadfa18d37db88f5b7053f3e82b7
MarcinZbor <marcinzb@gazeta.pl>
2023-02-13 13:44:18 +0100

    commit2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,38 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
+const galleryContainer = document.querySelector(".gallery");
+const imageMarkup = createItemsMarkup(galleryItems);
+galleryContainer.insertAdjacentHTML("beforeend", imageMarkup);
 
-console.log(galleryItems);
+function createItemsMarkup(images) {
+  return images
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+        <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+        />
+        </a>
+        </div>`;
+    })
+    .join("");
+}
+
+const clickContainer = (e) => {
+  e.preventDefault();
+
+  if (e.target.classList.contains("gallery")) return;
+  const source = e.target.dataset.source;
+
+  const instance = basicLightbox.create(`
+    <div class="modal">
+        <img src="${source}"width="800" height="600">
+        
+    </div>`);
+
+  instance.show();
+};
+
+galleryContainer.addEventListener("click", clickContainer);
